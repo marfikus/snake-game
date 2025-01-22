22f829d6488812fb1c1021c3718f3004619b05d2
Alexey Zaytsev <alex.rv9rh@gmail.com>
2025-01-22 22:03:07 +0500

    Add main function

--- main.py
+++ main.py
@@ -4,53 +4,57 @@ from eat import Eat
 from stone import Stone
 
 
-map = Map()
-map.map[0][1].content = Eat()
-map.map[0][3].content = Eat()
-map.map[5][5].content = Eat()
-map.map[7][1].content = Eat()
-map.map[7][9].content = Eat()
+def main():
+    map = Map()
+    map.map[0][1].content = Eat()
+    map.map[0][3].content = Eat()
+    map.map[5][5].content = Eat()
+    map.map[7][1].content = Eat()
+    map.map[7][9].content = Eat()
 
-snake = Snake("Snake1")
-map.add_snake(snake)
-map.show()
+    snake = Snake("Snake1")
+    map.add_snake(snake)
+    map.show()
 
-# snake.move("down")
-# snake.move("down")
-# snake.move("down")
-# snake.move("down")
-# snake.move("down")
-# snake.move("down")
-# snake.move("down")
-# snake.move("right")
-# snake.move("right")
-# snake.move("right")
-# snake.move("right")
-# snake.move("right")
-# snake.move("up")
-# snake.move("up")
-# snake.move("up")
-# snake.move("left")
-# snake.move("left")
-# snake.move("up")
-# snake.move("up")
-# snake.move("up")
-# snake.move("up")
-# snake.move("up")
+    # snake.move("down")
+    # snake.move("down")
+    # snake.move("down")
+    # snake.move("down")
+    # snake.move("down")
+    # snake.move("down")
+    # snake.move("down")
+    # snake.move("right")
+    # snake.move("right")
+    # snake.move("right")
+    # snake.move("right")
+    # snake.move("right")
+    # snake.move("up")
+    # snake.move("up")
+    # snake.move("up")
+    # snake.move("left")
+    # snake.move("left")
+    # snake.move("up")
+    # snake.move("up")
+    # snake.move("up")
+    # snake.move("up")
+    # snake.move("up")
 
-while True:
-    com = input()
-    if com == "q":
-        break
-    elif com == "s":
-        result = snake.move("left")
-    elif com == "f":
-        result = snake.move("right")
-    elif com == "e":
-        result = snake.move("up")
-    elif com == "d":
-        result = snake.move("down")
-    # map.show()
-    # if result == False:
-        # break
+    while True:
+        com = input()
+        if com == "q":
+            break
+        elif com == "s":
+            result = snake.move("left")
+        elif com == "f":
+            result = snake.move("right")
+        elif com == "e":
+            result = snake.move("up")
+        elif com == "d":
+            result = snake.move("down")
+        # map.show()
+        # if result == False:
+            # break
 
+
+if __name__ == "__main__":
+    main()
